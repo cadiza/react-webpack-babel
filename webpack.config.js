b013d672ee1b080c9e3c9cc34d00e8da78f1296b
Carlos <cdiaz@zaizi.com>
2021-02-10 14:10:06 +0000

    React with babel and webpack setup

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+module.exports = {
+  devServer: {
+    historyApiFallback: true,
+    contentBase: path.resolve(__dirname, './src'),
+  }, 
+  entry: path.resolve(__dirname, './src/index.js'), 
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        use: 'babel-loader'
+      }
+    ]
+  },
+  output: {
+    filename: 'bundle.js'
+  }
+
+}
